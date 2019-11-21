b811e109cda3ca36ff7e19f347a11c8f72b82bde
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-11-21 10:42:28 +0100

    add firebase

---
+++ src/environment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCxAHGZ11qjNNPI0dQsnMsEN2-RJkdhjUk",
+    authDomain: "hey-hey-wiki.firebaseapp.com",
+    databaseURL: "https://hey-hey-wiki.firebaseio.com",
+    projectId: "hey-hey-wiki",
+    storageBucket: "hey-hey-wiki.appspot.com",
+    messagingSenderId: "204569783618",
+    appId: "1:204569783618:web:bb8cf5f112c2b21c3b2cdb"
+};
