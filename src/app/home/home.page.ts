1407be35529c6c424055008d359b2282120f0785
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-12-12 08:04:49 +0100

    impl firestore service WIP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FirestoreService} from '../firestore.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {
+  constructor(private firestore: FirestoreService) {}
+
+  ngOnInit(): void {
+    this.firestore.getSubWikis().subscribe(value => console.log(value[0]));
   }
 }
+
+
