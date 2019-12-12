b34a425d08fc3d2a1ad35622488e0c66fb13a477
Josh Koroluk <joshua.koroluk@gmail.com>
2019-12-12 11:23:39 +0100

    Designed home page with limited functionality.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FirestoreService } from '../firestore.service';
 import { MenuController } from '@ionic/angular';
+import { SubWiki } from '../_models/sub-wiki';
 
 @Component({
   selector: 'app-home',
@@ -9,16 +10,37 @@ import { MenuController } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
+  private subwikis: SubWiki[];
+  public searchableSubwikis: SubWiki[];
+
   constructor(
     private firestore: FirestoreService,
     private menuController: MenuController,
   ) { }
 
   ngOnInit(): void {
-    this.firestore.getSubWikis().subscribe(value => console.log(value));
+    this.firestore.getSubWikis().subscribe(subwikis => {
+      this.subwikis = subwikis;
+      this.searchableSubwikis = this.subwikis;
+    });
   }
 
   ionViewWillEnter() {
     this.menuController.enable(false);
+  }
+
+  onSubwikiSearchChange(event: any) {
+    this.searchableSubwikis = this.subwikis;
+    const searchTerm = event.target.value;
+
+    if (searchTerm.trim() != '') {
+      this.searchableSubwikis = this.searchableSubwikis.filter(searchableSubwiki => {
+        return (searchableSubwiki.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  openSubwiki() {
+    console.log('opening');
   }
 }
